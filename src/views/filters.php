4c72b29ea98f9c46f1a47c7824b4fb61d599b6a5
LostCore <lostcore@gmail.com>
2017-02-21 17:31:42 +0100

    Initialized form

--- src/views/filters.php
+++ src/views/filters.php
@@ -1,4 +1,5 @@
 <div class="wbwpf-filters" data-filters>
+    <form method="post" action="" data-filters-form>
 	<?php if($has_filters): ?>
         <?php foreach ($filters as $filter): ?>
             <?php $filter->display(); ?>
@@ -6,4 +7,6 @@
     <?php else: ?>
         <?php _e("No filters defined in calling function.",$textdomain); ?>
     <?php endif; ?>
+        <button type="submit"><?php _ex("Search","Filters search button",$textdomain); ?></button>
+    </form>
 </div>
