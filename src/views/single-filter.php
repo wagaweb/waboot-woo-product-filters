9b56bcbe17fc4111b00508f1fece350620a28526
IacopoLea <iacopo@waga.it>
2017-03-13 11:12:14 +0100

    added wrapper

--- src/views/single-filter.php
+++ src/views/single-filter.php
@@ -1,8 +1,10 @@
 <div class=wbwpf-filter-wrapper data-filter='<?php echo $slug; ?>'<?php if($display_hidden): ?> style="display: none;"<?php endif; ?>>
     <h3><?php echo $label; ?></h3>
-    <?php echo $content; ?>
-    <input type='hidden' name='wbwpf_active_filters[<?php echo $slug; ?>][slug]' value='<?php echo $slug; ?>'>
-    <input type='hidden' name='wbwpf_active_filters[<?php echo $slug; ?>][type]' value='<?php echo $uiType; ?>'>
-    <input type='hidden' name='wbwpf_active_filters[<?php echo $slug; ?>][dataType]' value='<?php echo $dataType; ?>'>
+    <div class="wbwpf-input-wrapper">
+        <?php echo $content; ?>
+        <input type='hidden' name='wbwpf_active_filters[<?php echo $slug; ?>][slug]' value='<?php echo $slug; ?>'>
+        <input type='hidden' name='wbwpf_active_filters[<?php echo $slug; ?>][type]' value='<?php echo $uiType; ?>'>
+        <input type='hidden' name='wbwpf_active_filters[<?php echo $slug; ?>][dataType]' value='<?php echo $dataType; ?>'>
+    </div>
 </div>
 
