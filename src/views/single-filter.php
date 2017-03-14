19e43ea3ebfcac4a2045580aa88f458b08469256
LostCore <lostcore@gmail.com>
2017-03-14 13:02:53 +0100

    Added class quotes

--- src/views/single-filter.php
+++ src/views/single-filter.php
@@ -1,4 +1,4 @@
-<div class=wbwpf-filter-wrapper data-filter='<?php echo $slug; ?>'<?php if($display_hidden): ?> style="display: none;"<?php endif; ?>>
+<div class="wbwpf-filter-wrapper" data-filter='<?php echo $slug; ?>'<?php if($display_hidden): ?> style="display: none;"<?php endif; ?>>
     <h3><?php echo $label; ?></h3>
     <div class="wbwpf-input-wrapper">
         <?php echo $content; ?>
